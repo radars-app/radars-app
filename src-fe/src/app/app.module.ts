07837c92290fae1e4a250a80ecd7b9a68772d3a1
Maksim Kalachou <Maksim_Kalachou@epam.com>
2020-10-22 14:51:32 +0300

    feat(fe): add ngrx store and effects

--- src-fe/src/app/app.module.ts
+++ src-fe/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument(
+      {
+        maxAge: 25,
+        logOnly: environment.production,
+      },
+    ),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
