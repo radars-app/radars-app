5f9191914b4ca801fde0537bc766a6925359240a
Maksim Kalachou <Maksim_Kalachou@epam.com>
2020-11-19 18:20:20 +0300

    fix(fe): add fullWidth prop to buttons

--- src-fe/src/app/libs/common-components/button/button.component.ts
+++ src-fe/src/app/libs/common-components/button/button.component.ts
@@ -26,6 +26,9 @@ export class ButtonComponent {
 	@Input()
 	public disableHover: boolean = false;
 
+	@Input()
+	public fullWidth: boolean = false;
+
 	public get isDarkTheme(): boolean {
 		return this.theme === ComponentTheme.Dark;
 	}
