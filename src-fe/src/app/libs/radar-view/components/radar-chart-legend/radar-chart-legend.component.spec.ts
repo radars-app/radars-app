14a70da434cff92a034e3b7a1e10149c21c56d25
Maksim Kantsavy <MKantsavy@slb.com>
2020-11-28 17:13:33 +0300

    feat: add theming to radar-chart-legend

--- src-fe/src/app/libs/radar-view/components/radar-chart-legend/radar-chart-legend.component.spec.ts
+++ src-fe/src/app/libs/radar-view/components/radar-chart-legend/radar-chart-legend.component.spec.ts
@@ -1,7 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { CommonComponentsModule } from 'src/app/libs/common-components/common-components.module';
+import { ComponentTheme } from 'src/app/libs/common-components/common/enum/component-theme.enum';
 import { IconService } from 'src/app/libs/common-components/icon/service/icon.service';
+import { ContainerFacadeService } from 'src/app/libs/container/service/container-facade.service';
 
 import { RadarChartLegendComponent } from './radar-chart-legend.component';
 
@@ -12,6 +15,14 @@ describe('RadarChartLegendComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [RadarChartLegendComponent],
+			providers: [
+				{
+					provide: ContainerFacadeService,
+					useValue: {
+						theme$: of(ComponentTheme.Light),
+					},
+				},
+			],
 			imports: [CommonComponentsModule, HttpClientModule],
 		}).compileComponents();
 	}));
