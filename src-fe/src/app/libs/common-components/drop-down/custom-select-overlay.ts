e4d5908bce6de5f3a2b31374b3ec7befc0eac57b
Uladzimir Svirydzenka <57321079+vladimir-sviridenko@users.noreply.github.com>
2021-01-04 14:07:27 +0300

    refactor: remove extra comments

--- src-fe/src/app/libs/common-components/drop-down/custom-select-overlay.ts
+++ src-fe/src/app/libs/common-components/drop-down/custom-select-overlay.ts
@@ -1,16 +1,9 @@
 import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 
-/**
- * Extending the `@angular/cdk` overlay to add a custom class when it is used by the mat-select
- */
 export class CustomSelectOverlay extends Overlay {
 	private readonly OVERLAY_PANEL_CLASS: string = 'drop-down__overlay-panel';
 
-	/**
-	 * Creates an overlay that will also add the custom overlay panel class to the overlay panel.
-	 */
 	public create(config?: OverlayConfig): OverlayRef {
-		// Add to existing config
 		if (Boolean(config) && Boolean(config.panelClass)) {
 			if (Array.isArray(config.panelClass)) {
 				config.panelClass.push(this.OVERLAY_PANEL_CLASS);
@@ -18,7 +11,6 @@ export class CustomSelectOverlay extends Overlay {
 				config.panelClass = [config.panelClass, this.OVERLAY_PANEL_CLASS];
 			}
 		} else {
-			// Create new config
 			if (config) {
 				config.panelClass = [this.OVERLAY_PANEL_CLASS];
 			} else {
