4de31d2a4e9a25a573526877f3a333d1fbed5c9e
Maksim Kantsavy <MKantsavy@slb.com>
2021-01-15 17:36:33 +0300

    fix: lint

--- src-fe/src/app/libs/container/components/global-header/global-header.component.spec.ts
+++ src-fe/src/app/libs/container/components/global-header/global-header.component.spec.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { CommonComponentsModule } from 'src/app/libs/common-components/common-components.module';
 import { IconService } from 'src/app/libs/common-components/icon/service/icon.service';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
