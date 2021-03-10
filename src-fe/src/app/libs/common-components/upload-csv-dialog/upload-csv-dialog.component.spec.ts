92f546165b52a68552e458cb0fa9cd146544448c
Maksim Kantsavy <MKantsavy@slb.com>
2021-03-10 15:40:46 +0300

    fix: reset zoom state on resize

--- src-fe/src/app/libs/common-components/upload-csv-dialog/upload-csv-dialog.component.spec.ts
+++ src-fe/src/app/libs/common-components/upload-csv-dialog/upload-csv-dialog.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonComponentsModule } from '../common-components.module';
+import { IconService } from '../icon/service/icon.service';
 
 import { UploadCsvDialogComponent } from './upload-csv-dialog.component';
 
@@ -9,10 +13,13 @@ describe('UploadCsvDialogComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [UploadCsvDialogComponent],
+			imports: [CommonComponentsModule, BrowserAnimationsModule, HttpClientModule],
 		}).compileComponents();
 	}));
 
 	beforeEach(() => {
+		const iconService: IconService = TestBed.inject(IconService);
+		iconService.addIcons();
 		fixture = TestBed.createComponent(UploadCsvDialogComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
